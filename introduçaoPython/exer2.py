468907d14066505aedf982015eb31c7fa26e3773
BalhegoJ <jvbbalhego@gmail.com>
2024-08-08 00:52:46 -0300

    implementação exercicio 2

---
+++ introduçaoPython/exer2.py
@@ -0,0 +1,5 @@
+num1 = int(input('Digite o primero número: '))
+num2 = int(input('Digite o segundo número: '))
+num3 = int(input('Digite o terceiro número: '))
+
+print('A soma dos três números é ', format(num1+num2+num3), 'e a media deles é ', format((num1+num2+num3)/3))
