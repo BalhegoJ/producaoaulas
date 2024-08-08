d18c67699f8b52c8dffbbd4f168753426c9f90be
BalhegoJ <jvbbalhego@gmail.com>
2024-08-08 00:54:25 -0300

    implementando exercicio 4

---
+++ introduçaoPython/exer4.py
@@ -0,0 +1,6 @@
+base = int(input('informe a base do triângulo: '))
+altura = int(input('informe a altura do triângulo: '))
+
+area = (base * altura) / 2
+
+print ('A area do triângulo é:', area, 'metros quadrados')
